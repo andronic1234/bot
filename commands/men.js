d905cc4e43f6b056228595a149790f53ea5bc9d4
andronic1234 <fotopoulos.andrew@gmail.com>
2021-12-05 15:42:34 +0200

    Update men.js

--- commands/men.js
+++ commands/men.js
@@ -5,6 +5,8 @@ module.exports = {
     async execute(client, message, cmd, args, Discord, profileData) {
         // const men = require('../men.json');
             if(cmd === 'men'){
+            message.delete(200);
+            if(!profileData) return;
             let the = message.author.username;
             message.channel.send(`**${the}** You have said men a total of **${profileData.men}** times! Letsgoooo!!`);
         }
