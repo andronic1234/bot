fa6b00be089dc2f0aa75178ecaa4f8b4f4313c68
andronic1234 <fotopoulos.andrew@gmail.com>
2021-12-05 17:13:05 +0200

    Update leaderboard.js

--- commands/leaderboard.js
+++ commands/leaderboard.js
@@ -22,7 +22,7 @@ module.exports = {
     
             const Lead = new MessageEmbed()
             .setColor('DARK_GREEN')
-            .setThumbnail("https://cdn.discordapp.com/attachments/755482197150007448/912481617950437437/GoDSlayeRLogoLOW2.png")
+            .setAuthor("https://cdn.discordapp.com/attachments/755482197150007448/912481617950437437/GoDSlayeRLogoLOW2.png")
             .setTitle('The Men Leaderboard')
             .setTimestamp()
 
