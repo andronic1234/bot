875914ffafbb7ede251e34da4e5f1f9e9f43091a
andronic1234 <fotopoulos.andrew@gmail.com>
2021-12-07 12:51:51 +0200

    a

--- events/guild/message.js
+++ events/guild/message.js
@@ -19,16 +19,6 @@ module.exports = async (Discord, client, message) => {
         console.log(err)
     }
 
-    // let profileBoard;
-    // try{
-    //     profileBoard = await profileModel.find({});
-
-
-    // }catch(err){
-    //     console.log(err)
-    // }
-
-
     const args = message.content.slice(prefix.length).split(/ +/);
     const cmd = args.shift().toLowerCase();
 
