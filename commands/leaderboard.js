28aebb59a466dd381eedb5613f33bfaf2e576ccd
andronic1234 <fotopoulos.andrew@gmail.com>
2021-12-07 13:25:39 +0200

    Update leaderboard.js

--- commands/leaderboard.js
+++ commands/leaderboard.js
@@ -15,7 +15,7 @@ module.exports = {
             let members = []
 
             for (let obj of profileBoard) {
-                if(guild.members.cache
+                if(message.guild.members.cache
                 .map((member) => member.id)
                 .includes(obj.userID)) members.push(obj)
             }
@@ -45,7 +45,7 @@ module.exports = {
             members = members.slice(0, 10)
             let desc = ""
             for(let i = 0; i < members.length; i++) {
-                let user = client.users.cache.get(members[i].userID)
+                let user = client.guild.members.cache.get(members[i].userID)
                 if(!user) return
                 let val = members[i].men
                 desc += `**${i +1}.** __${user.tag}__ - ${val}\n`
